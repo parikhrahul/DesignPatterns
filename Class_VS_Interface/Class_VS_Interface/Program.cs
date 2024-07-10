f9e1347d845bb246041ea8bdd48a8365a2a41e61
Rahul Parikh <RParikh@corp.mm-software.com>
2024-07-10 19:08:56 +0530

    Class_VS_Interface

---
+++ Class_VS_Interface/Class_VS_Interface/Program.cs
@@ -0,0 +1,45 @@
+﻿using Class_VS_Interface;
+using System;
+
+//namespace Class_VS_Interface
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            var eich = new Brendan();
+//            var sanjay = new Sanjay();
+
+//            eich.eat();
+//            eich.sleep();
+//            eich.love();
+//            eich.run();
+//            eich.writeCode();
+
+//            sanjay.playMusic();
+//            sanjay.writeCode();
+
+//            Console.ReadLine();
+
+//        }
+//    }
+//}
+
+//ILDASM
+
+Brendan eich = new Brendan();
+Sanjay sanjay = new Sanjay();
+
+
+eich.eat();
+eich.sleep();
+eich.love();
+eich.run();
+eich.writeCode();
+
+sanjay.playMusic();
+sanjay.writeCode();
+
+Console.ReadLine();
+
+
---
+++ Class_VS_Interface/Class_VS_Interface/classiface.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_VS_Interface
+{
+    public class Human
+    {
+        //* Method defination
+        public void eat() { Console.WriteLine("Chomp! Chomp!"); }
+        public void sleep() { Console.WriteLine("zzzzZZZZZ"); }
+        public void love() { Console.WriteLine("I love India"); }
+    }
+
+    public interface IDeveloper
+    {
+        // Method declaration
+        void writeCode();
+    }
+
+    public interface IMusician
+    {
+        void playMusic();
+    }
+
+    public class Brendan : Human, IDeveloper
+    {
+        public void writeCode()
+        {
+            Console.WriteLine("Develop JavaScript Language");
+        }
+
+        public void run()
+        {
+            Console.WriteLine("Run run run!!!");
+        }    
+    }
+
+    public class Sanjay : Human, IMusician, IDeveloper
+    {
+        public void writeCode() {
+
+            Console.WriteLine("Develop core code of toll plaza application");
+        }
+
+        public void playMusic()
+        {
+            Console.WriteLine(" Sing Rag Malhar");
+        }
+    }
+
+}
